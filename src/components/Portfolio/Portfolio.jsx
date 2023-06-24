976a8de6386df6dfb3a64020806d24704e02c7d4
Kehbuma15 <kehbuma185@gmail.com>
2023-06-24 12:13:15 +0100

    updated code

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,90 +1,90 @@
-// import React from 'react'
-// import './portfolio.css'
-// import IMG1 from '../../assets/image1.webp'
-// import IMG2 from '../../assets/image3.webp'
-// import IMG3 from '../../assets/image4.webp'
-// import IMG4 from '../../assets/image5.webp'
-// import IMG5 from '../../assets/image6.webp'
-// import IMG6 from '../../assets/image7.webp'
+import React from 'react'
+import './portfolio.css'
+import IMG1 from '../../assets/image1.webp'
+import IMG2 from '../../assets/image3.webp'
+import IMG3 from '../../assets/image4.webp'
+import IMG4 from '../../assets/image5.webp'
+import IMG5 from '../../assets/image6.webp'
+import IMG6 from '../../assets/image7.webp'
 
-// const data = [
-//    {
-//    id: 1,
-//    image: IMG1,
-//    title: 'Figma design',
-//    github: 'https://github.com',
-//    demo: 'https://dribbble.com/Alien_pixels'
-//    },
-//    {
-//       id: 2,
-//       image: IMG2,
-//       title: 'Library Management Creation',
-//       github: 'https://github.com',
-//       demo: 'https://dribbble.com/Alien_pixels'
-//       },
+const data = [
+   {
+   id: 1,
+   image: IMG1,
+   title: 'Figma design',
+   github: 'https://github.com',
+   demo: 'https://dribbble.com/Alien_pixels'
+   },
+   {
+      id: 2,
+      image: IMG2,
+      title: 'Library Management Creation',
+      github: 'https://github.com',
+      demo: 'https://dribbble.com/Alien_pixels'
+      },
      
 
-//          {
-//             id: 3,
-//             image: IMG3,
-//             title: 'Beauty Salon Posters',
-//             github: 'https://github.com',
-//             demo: 'https://dribbble.com/Alien_pixels'
-//             },
+         {
+            id: 3,
+            image: IMG3,
+            title: 'Beauty Salon Posters',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/Alien_pixels'
+            },
 
-//             {
-//                id: 4,
-//                image: IMG4,
-//                title: 'A html and css page for building',
-//                github: 'https://github.com',
-//                demo: 'https://dribbble.com/Alien_pixels'
-//                },
+            {
+               id: 4,
+               image: IMG4,
+               title: 'A html and css page for building',
+               github: 'https://github.com',
+               demo: 'https://dribbble.com/Alien_pixels'
+               },
 
-//                {
-//                   id: 5,
-//                   image: IMG5,
-//                   title: 'An Organisation planning app',
-//                   github: 'https://github.com',
-//                   demo: 'https://dribbble.com/Alien_pixels'
-//                   },
-//                   {
-//                      id: 6,
-//                      image: IMG6,
-//                      title: 'Health management system',
-//                      github: 'https://github.com',
-//                      demo: 'https://dribbble.com/Alien_pixels'
-//                      },
+               {
+                  id: 5,
+                  image: IMG5,
+                  title: 'An Organisation planning app',
+                  github: 'https://github.com',
+                  demo: 'https://dribbble.com/Alien_pixels'
+                  },
+                  {
+                     id: 6,
+                     image: IMG6,
+                     title: 'Health management system',
+                     github: 'https://github.com',
+                     demo: 'https://dribbble.com/Alien_pixels'
+                     },
                   
 
-// ]
-// const Portfolio = () => {
-//     return(
-//         <section id = 'portfolio'>
-//             <h5>My recent work</h5>
-//                 <h2>Portfolio</h2>
-//             <div className="container portfolio__container">
-//                {
-//                   data.map(({id, image, title, github, demo}) =>{
-//                      return(
-//                         <article key={id} className='portfolio__item'>
-//                            <div className ="portfolio__item-image">
-//                            <img src={image} alt={title} />
-//                            </div>
-//                            <h3>{title}</h3>
-//                            <div className="portfolio__item-cta">
-//                            <a href = {github} className='btn'>Github</a>
-//                            <a href = {demo} className='btn btn-primary' target='_blank'>Live Demo</a>
-//                            </div>
+]
+const Portfolio = () => {
+    return(
+        <section id = 'portfolio'>
+            <h5>My recent work</h5>
+                <h2>Portfolio</h2>
+            <div className="container portfolio__container">
+               {
+                  data.map(({id, image, title, github, demo}) =>{
+                     return(
+                        <article key={id} className='portfolio__item'>
+                           <div className ="portfolio__item-image">
+                           <img src={image} alt={title} />
+                           </div>
+                           <h3>{title}</h3>
+                           <div className="portfolio__item-cta">
+                           <a href = {github} className='btn'>Github</a>
+                           <a href = {demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                           </div>
                   
-//                               </article>
-//                      )
-//                   })
-//                }
+                              </article>
+                     )
+                  })
+               }
                 
-//             </div>
-//         </section>
-//     )
-// }
-// export default Portfolio
+            </div>
+        </section>
+    )
+}
+export default Portfolio
 
 
